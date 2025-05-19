0c864cfd998a79ea81201a7962f5c014e71b5424
jdnjsy <jdnjsyoo@snu.ac.kr>
2025-05-19 20:02:24 +0900

    2.

--- account/views.py
+++ account/views.py
@@ -5,7 +5,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
 from account.request_serializers import SignInRequestSerializer, SignUpRequestSerializer, TokenRefreshRequestSerializer
 
 from .serializers import (
@@ -115,3 +117,32 @@ class TokenRefreshView(APIView):
         response = Response({"detail": "token refreshed"}, status=status.HTTP_200_OK)
         response.set_cookie("access_token", value=str(new_access_token), httponly=True)
         return response
+
+class SignOutView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    @swagger_auto_schema(
+        operation_id="로그아웃",
+        operation_description="로그아웃을 진행합니다.",
+        request_body=TokenRefreshRequestSerializer,
+        responses={200: "No Content", 401: "Unauthorized", 400: "Bad Request"},
+    )
+    
+    def post(self, request):
+        refresh_token = request.data.get("refresh")
+        
+        if not refresh_token:
+            return Response(
+                {"detail": "no refresh token"},
+                status=status.HTTP_400_BAD_REQUEST)
+            
+        try:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        
+        except TokenError:
+            return Response(
+                {"detail": "please signin"},
+                status=status.HTTP_401_UNAUTHORIZED)
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
