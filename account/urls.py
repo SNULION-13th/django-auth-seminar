733e6ccae4f3c4ffaabf17b85d6c8d32e28a4e29
teireby <dongin1001@snu.ac.kr>
2025-05-19 20:01:11 +0900

    9주차 과제_박동인인

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import SignUpView, SignInView
+### 🔻 TokenRefreshView 추가 ####
+from .views import SignInView, SignUpView, TokenRefreshView, LogOutView
+### 🔺 이 부분만 추가 ####
 
 
 app_name = 'account'
@@ -7,4 +10,8 @@ urlpatterns = [
     # CBV url path
     path("signup/", SignUpView.as_view()),
     path("signin/", SignInView.as_view()),
+### 🔻 이 부분만 추가 ####
+    path("refresh/", TokenRefreshView.as_view()),
+### 🔺 이 부분만 추가 ####
+    path("logout/", LogOutView.as_view()),
 ]
--- account/views.py
+++ account/views.py
@@ -5,8 +5,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+from account.request_serializers import SignInRequestSerializer, SignUpRequestSerializer, TokenRefreshRequestSerializer, LogoutRequestSerializer
 
-from account.request_serializers import SignInRequestSerializer, SignUpRequestSerializer
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken
 
 from .serializers import (
     UserSerializer,
@@ -14,6 +15,16 @@ from .serializers import (
 )
 from .models import UserProfile
 
+from rest_framework_simplejwt.tokens import RefreshToken #추가
+
+def set_token_on_response_cookie(user, status_code):
+    token = RefreshToken.for_user(user)
+    user_profile = UserProfile.objects.get(user=user)
+    serialized_data = UserProfileSerializer(user_profile).data
+    res = Response(serialized_data, status=status_code)
+    res.set_cookie("refresh_token", value=str(token), httponly=True)
+    res.set_cookie("access_token", value=str(token.access_token), httponly=True)
+    return res
 class SignUpView(APIView):
     @swagger_auto_schema(
           operation_id="회원가입",
@@ -34,9 +45,10 @@ class SignUpView(APIView):
         user_profile = UserProfile.objects.create(
             user=user, college=college, major=major
         )
-        user_profile_serializer = UserProfileSerializer(instance=user_profile)
-        return Response(user_profile_serializer.data, status=status.HTTP_201_CREATED)
-
+### 🔻 이 부분만 변경 ####
+        return set_token_on_response_cookie(user, status_code=status.HTTP_201_CREATED)
+### 🔺 이 부분만 변경 ####
+        
 
 class SignInView(APIView):
     @swagger_auto_schema(
@@ -60,11 +72,83 @@ class SignInView(APIView):
                     {"message": "Password is incorrect"},
                     status=status.HTTP_400_BAD_REQUEST,
                 )
-            user_profile = UserProfile.objects.get(user=user)
-            user_profile_serializer = UserProfileSerializer(instance=user_profile)
-            return Response(user_profile_serializer.data, status=status.HTTP_200_OK)
+### 🔻 이 부분만 변경 ####
+            return set_token_on_response_cookie(user, status_code=status.HTTP_200_OK)
+### 🔺 이 부분만 변경 ####
 
         except User.DoesNotExist:
             return Response(
                 {"message": "User does not exist"}, status=status.HTTP_404_NOT_FOUND
             )
+
+
+#----------------------------------------#
+
+
+class TokenRefreshView(APIView):
+    @swagger_auto_schema(
+        operation_id="토큰 재발급",
+        operation_description="access 토큰을 재발급 받습니다.",
+        request_body=TokenRefreshRequestSerializer,
+        responses={200: UserProfileSerializer},
+    )
+    def post(self, request):
+        refresh_token = request.data.get("refresh")
+        
+        #### 1
+        if not refresh_token:
+            return Response(
+                {"detail": "no refresh token"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+        #### 2
+            RefreshToken(refresh_token).verify()
+        except:
+            return Response(
+                {"detail": "please signin again."}, status=status.HTTP_401_UNAUTHORIZED
+            )
+            
+        #### 3
+        new_access_token = str(RefreshToken(refresh_token).access_token)
+        response = Response({"detail": "token refreshed"}, status=status.HTTP_200_OK)
+        response.set_cookie("access_token", value=str(new_access_token), httponly=True)
+        return response
+    
+class LogOutView(APIView):
+    @swagger_auto_schema(
+        operation_id="로그아웃",
+        operation_description="로그아웃을 진행합니다.",
+        request_body=LogoutRequestSerializer,
+        responses={204: "No Content", 400: "Bad Request", 401: "Unauthorized"},
+    )
+    def post(self, request):
+        #리프레시 토큰 전달 안한경우.
+        refresh_token = request.data.get("refresh")
+        if not refresh_token:
+            return Response(
+                {"detail": "no refresh token"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        #일단 verify를 통해 유효한 토큰인지 확인(이미 블랙리스트에 올라가있는지도 검사.)
+        try:
+            RefreshToken(refresh_token).verify()
+        except:
+            return Response(
+                {"detail": "please signin"}, status=status.HTTP_401_UNAUTHORIZED
+            )
+        
+        #혹시나 outstanding token에 존재안할 수도 있으니 확인.
+        try:
+            black_token = OutstandingToken.objects.get(token=refresh_token)
+            BlacklistedToken.objects.create(token=black_token)
+        except OutstandingToken.DoesNotExist:
+            return Response(
+                {"detail": "please signin"}, status=status.HTTP_401_UNAUTHORIZED
+            )
+        
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+#----------------------------------------#
