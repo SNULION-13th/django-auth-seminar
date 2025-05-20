43e48f7c268175a963368c5fa337855f2b0876f1
paul2126 <paul.mhryu@gmail.com>
2025-05-20 20:33:40 +0900

    add signout function

--- account/request_serializers.py
+++ account/request_serializers.py
@@ -17,3 +17,7 @@ class SignInRequestSerializer(serializers.Serializer):
 
 class TokenRefreshRequestSerializer(serializers.Serializer):
     refresh = serializers.CharField()
+
+
+class SignOutRequestSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
